2b7b264f3e163f60c9169d2a6549570413e710da
Rick <rickoliwer@gmail.com>
2021-10-13 09:32:06 +0200

    footer done

--- wineo/src/components/layout/footer/index.js
+++ wineo/src/components/layout/footer/index.js
@@ -1,33 +1,41 @@
 import {isEmpty, isArray} from 'lodash';
 import {sanitize} from '../../../utils/miscellaneous';
 import Link from 'next/link'
+import Image from "next/image"
+import footerLogo from "../../../../public/images/footer/footer-logo.png"
 const Footer = ({ footer, footerMenus }) => {
 
     return (
         <footer className="p-6 text-white bg-brand-dark">
-            <div className="grid w-full grid-cols-1 text-center md:gap-9 md:grid-cols-3 md:text-left">
+            <div>
+                <div className="grid w-full grid-cols-1 text-center border-b border-white md:gap-32 md:grid-cols-3 md:text-left">
 
-                <div>
-                    Hello world
+                    <div>
+                        <div className="relative w-4/6 m-auto mt-5 h-4/6">
+                            <Image layout="responsive" objectFit="cover" src={footerLogo} priority/>
+                            <p className="text-sm text-center text-white my-9">Guiding the way to enjoyment of life since 2009</p>
+                        </div>
+                    </div>
+
+                    <div className="wtf">
+                        <div className="side_one" dangerouslySetInnerHTML={{ __html: sanitize(footer?.sidebarOne)}} />
+                    </div>
+
+                    <div>
+                        <div className="side_two" dangerouslySetInnerHTML={{ __html: sanitize(footer?.sidebarTwo)}} />
+                    </div>
+                </div>
+                <div className="grid w-full grid-cols-2 py-4">
+
+                    <div class="text-left">
+                        {footer.copyrightText ? footer.copyrightText : ''}
+                    </div>
+
+                    <div className="text-right">
+                        Built with ♥ by Capace Media
+                    </div>
                 </div>
 
-                <div className="wtf">
-                    <div className="side_one" dangerouslySetInnerHTML={{ __html: footer?.sidebarOne}} />
-                </div>
-
-                <div>
-                    <div className="side_two" dangerouslySetInnerHTML={{ __html: sanitize(footer?.sidebarTwo)}} />
-                </div>
-            </div>
-            <div className="grid grid-cols-2">
-
-                <div class="text-left">
-                    {footer.copyrightText ? footer.copyrightText : ''}
-                </div>
-
-                <div className="text-right">
-                Built with ♥ by Capace Media
-                </div>
             </div>
         </footer>
     )
